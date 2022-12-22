7a5d4e94f89ab03741e0492fbb70f0a829138d9e
kaio.salgado <kaio.salgado@placeti.com.br>
2022-12-22 17:30:30 -0300

    Implementação de formulário reativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { FormPensamentoComponent } from './componentes/pensamentos/form-pensamento/form-pensamento.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { PensamentoCardComponent } from './componentes/pensamentos/pensamento-card/pensamento-card.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -26,7 +26,8 @@ import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pe
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componentes/pensamentos/form-pensamento/form-pensamento.component.ts
+++ src/app/componentes/pensamentos/form-pensamento/form-pensamento.component.ts
@@ -3,38 +3,51 @@ import { Location } from '@angular/common';
 import { PensamentoService } from '../pensamento.service';
 import { Pensamento } from '../pensamento';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-form-pensamento',
   templateUrl: './form-pensamento.component.html',
-  styleUrls: ['./form-pensamento.component.css']
+  styleUrls: ['./form-pensamento.component.css'],
 })
 export class FormPensamentoComponent {
+  form!: FormGroup;
+  id = 0;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: PensamentoService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
-
-  constructor (private route: ActivatedRoute,private router: Router, private service: PensamentoService) {}
   ngOnInit() {
-    if(this.route.snapshot.paramMap.get('id')){
-      const id = this.route.snapshot.paramMap.get('id')
+    if (this.route.snapshot.paramMap.get('id')) {
+      const id = this.route.snapshot.paramMap.get('id');
       this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
-        this.pensamento = pensamento;
-      })
+        this.id = pensamento.id!;
+        this.form = this.formBuilder.group({
+          conteudo: [pensamento.conteudo],
+          autoria: [pensamento.autoria],
+          modelo: [pensamento.modelo],
+        });
+      });
+    } else {
+      this.form = this.formBuilder.group({
+        conteudo: ['Formulário reativo'],
+        autoria: ['Angular'],
+        modelo: ['modelo1'],
+      });
     }
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
+    this.service.criar(this.form.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
     });
   }
   alterarPensamento() {
-    this.service.alterar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
+    this.service.alterar(this.form.value).subscribe(() => {
+      this.router.navigate(['/listarPensamento']);
     });
   }
   cancelarPensamento() {
